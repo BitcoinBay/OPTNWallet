14d031e69d2f0fa1304cbe212636cc20c576939d
Uaena0593 <86070045+Uaena0593@users.noreply.github.com>
2024-05-01 12:21:31 -0400

    Next.js Setup with Capacitor
    All dependencies are installed and working for capacitor in the Nextjs app.

---
+++ optnwallet/android/app/src/main/java/com/bitcoinbay/OPTNWallet/MainActivity.java
@@ -0,0 +1,5 @@
+package com.bitcoinbay.OPTNWallet;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ optnwallet/capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bitcoinbay.OPTNWallet',
+  appName: 'OPTNWallet',
+  webDir: 'out',
+  "bundledWebRuntime": false
+};
+
+export default config;
