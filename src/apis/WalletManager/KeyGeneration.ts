22b169ea0f8d081beb556c30b6a7dacb31068eb1
Jonathan Chen <86070045+jonathanc-n@users.noreply.github.com>
2024-07-08 14:32:59 -0400

    P2PKH Contract Transaction Working

--- src/apis/WalletManager/KeyGeneration.ts
+++ src/apis/WalletManager/KeyGeneration.ts
@@ -3,7 +3,7 @@ import {
     deriveHdPath,
     secp256k1,
     encodeCashAddress,
-} from '@bitauth/libauth';
+} from '@bitauth/libauth-v3';
 import { hash160 } from '@cashscript/utils';
 import * as bip39 from "bip39";
 
@@ -20,29 +20,33 @@ export default function KeyGeneration() {
 
     async function generateKeys(mnemonic : string, passphrase : string, keyNumber : number) {
         const seed = await bip39.mnemonicToSeed(mnemonic, passphrase);
-        const rootNode = deriveHdPrivateNodeFromSeed(seed, true);
+        const rootNode = deriveHdPrivateNodeFromSeed(seed);
         const baseDerivationPath = `m/44'/145'/${keyNumber}'/0`;
-        // const baseDerivationPathChange = "m/44'/145'/0'/1";
-        // const changeNode = deriveHdPath(rootNode, `${baseDerivationPathChange}/${changeIndex}`);
-
         const aliceNode = deriveHdPath(rootNode, `${baseDerivationPath}/0`);
 
         if (typeof aliceNode === 'string') throw new Error();
+        
         const alicePub  = secp256k1.derivePublicKeyCompressed(aliceNode.privateKey);
         const alicePriv = aliceNode.privateKey;
+
         if (typeof alicePub === 'string') {
-            console.log("alicePub should not be a string.")
+            console.log("alicePub should not be a string.");
             return null;
         }
-        const alicePkh = hash160(alicePub);
-        console.log(alicePub);
-        console.log(alicePriv);
-        const aliceAddress = encodeCashAddress('bchtest', 'p2pkh', alicePkh);
 
-        // const changePub = secp256k1.derivePublicKeyCompressed(changeNode.privateKey);
-        // const changePriv = changeNode.privateKey;
-        // const changePkh = hash160(changePub);
-        // const changeAddress = encodeCashAddress('bchtest', 'p2pkh', changePkh);
-        return { alicePub, alicePriv, aliceAddress }
+        const alicePkh = hash160(alicePub);
+        if (!alicePkh) {
+            console.error('Failed to generate public key hash.');
+            return null;
+        }
+
+        const aliceAddress = encodeCashAddress({
+            payload: alicePkh,
+            prefix: 'bchtest',
+            type: 'p2pkh',
+        }).address;
+
+        console.log(aliceAddress);
+        return { alicePub, alicePriv, aliceAddress };
     };
 }
