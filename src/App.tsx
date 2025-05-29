36bebf68ae2b187c89aea4f52c9b2196da01b3fc
lightswarm124 <lightswarm@gmail.com>
2025-05-29 17:51:30 -0400

    cleaning up wc popups

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,8 @@ import {
 import CampaignDetail from './pages/apps/utils/CampaignDetail';
 import { initWalletConnect } from './redux/walletconnectSlice';
 import { usePrices } from './hooks/usePrices';
+import { SignTransactionModal } from './components/walletconnect/SignTransactionModal';
+import { SignMessageModal } from './components/walletconnect/SignMessageModal';
 
 let utxoWorkerStarted = false;
 let transactionWorkerStarted = false;
@@ -36,10 +38,10 @@ function App() {
   );
   const location = useLocation();
 
-    // 1) Initialize WalletConnect once
-    useEffect(() => {
-      dispatch(initWalletConnect())
-    }, [dispatch])
+  // 1) Initialize WalletConnect once
+  useEffect(() => {
+    dispatch(initWalletConnect());
+  }, [dispatch]);
 
   useEffect(() => {
     if (walletId === 1) {
@@ -81,42 +83,48 @@ function App() {
   }, [walletId, location.pathname]);
 
   return (
-    <Routes>
-      <Route path="/" element={<RootHandler />} />
-      {walletId === 1 ? (
-        <>
-          <Route element={<Layout />}>
-            <Route path="/home/:wallet_id" element={<Home />} />
-            <Route path="/contract" element={<ContractView />} />
-            <Route path="/apps" element={<AppsView />} />
-            <Route path="/apps/fundme" element={<AppFundMe />} />
-            <Route path="/campaign/:id" element={<CampaignDetail />} />
-            <Route path="/receive" element={<Receive />} />
-            <Route path="/transaction" element={<Transaction />} />
+    <>
+      <Routes>
+        <Route path="/" element={<RootHandler />} />
+        {walletId === 1 ? (
+          <>
+            <Route element={<Layout />}>
+              <Route path="/home/:wallet_id" element={<Home />} />
+              <Route path="/contract" element={<ContractView />} />
+              <Route path="/apps" element={<AppsView />} />
+              <Route path="/apps/fundme" element={<AppFundMe />} />
+              <Route path="/campaign/:id" element={<CampaignDetail />} />
+              <Route path="/receive" element={<Receive />} />
+              <Route path="/transaction" element={<Transaction />} />
+              <Route
+                path="/transactions/:wallet_id"
+                element={<TransactionHistory />}
+              />
+              <Route path="/settings" element={<Settings />} />
+            </Route>
             <Route
-              path="/transactions/:wallet_id"
-              element={<TransactionHistory />}
+              path="/"
+              element={<Navigate to={`/home/${walletId}`} replace />}
             />
-            <Route path="/settings" element={<Settings />} />
-          </Route>
-          <Route
-            path="/"
-            element={<Navigate to={`/home/${walletId}`} replace />}
-          />
-          <Route
-            path="*"
-            element={<Navigate to={`/home/${walletId}`} replace />}
-          />
-        </>
-      ) : (
-        <>
-          <Route path="/landing" element={<LandingPage />} />
-          <Route path="/createwallet" element={<CreateWallet />} />
-          <Route path="/importwallet" element={<ImportWallet />} />
-          <Route path="*" element={<Navigate to="/landing" replace />} />
-        </>
-      )}
-    </Routes>
+            <Route
+              path="*"
+              element={<Navigate to={`/home/${walletId}`} replace />}
+            />
+          </>
+        ) : (
+          <>
+            <Route path="/landing" element={<LandingPage />} />
+            <Route path="/createwallet" element={<CreateWallet />} />
+            <Route path="/importwallet" element={<ImportWallet />} />
+            <Route path="*" element={<Navigate to="/landing" replace />} />
+          </>
+        )}
+      </Routes>
+
+      {/* 🔥 This ensures the modals is always active */}
+      <SignMessageModal />
+      <SignTransactionModal />
+    </>
   );
 }
 
